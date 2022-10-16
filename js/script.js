93fae8ba8948d33f04ad3ddbc8a979536145505d
rizkywis12 <wisesarr@gmail.com>
2022-10-16 19:20:55 +0700

    push semua ke git, masih belum selesai

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+$(function() {
+    $("#dropdown a").click(function() {
+        $("#AdvancedSearch .selection").text('Advanced:' + $(this).text());
+    });
+});
